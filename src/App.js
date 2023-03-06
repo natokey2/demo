fb789896df9f082abf47ba5c00393c4a04f79966
natokey2 <113178345+natokey2@users.noreply.github.com>
2023-03-06 17:18:21 +0100

    hello

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+
+
+function App() {
+  return (
+    
+  
+    <>
+          <h1 className="title">c4a.dev</h1>
+      <h2>natiq alhawmani</h2>
+    </>
+   
+    
+
+  
+
+  );
+}
+
+export default App;
